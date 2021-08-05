acd33f9cb319ed299006183889fffe24eb50b893
Nuttavut Thongjor <n.thongjor@gmail.com>
2021-08-05 16:08:02 +0700

    add ability to test ui part

---
+++ cypress/models/category.ts
@@ -0,0 +1,16 @@
+import * as faker from 'faker';
+
+export type Category = {
+  id: number;
+  name: string;
+};
+
+export const createCategory = (category: Partial<Category> = {}) => {
+  const id = faker.datatype.number();
+
+  return {
+    id,
+    name: `${faker.lorem.word()}-${id}`,
+    ...category,
+  };
+};
---
+++ cypress/support/commands.ts
@@ -0,0 +1,43 @@
+import { RouteHandler } from 'cypress/types/net-stubbing';
+import { User } from 'models/user';
+import * as faker from 'faker';
+import { mockGetProfile } from 'api/auth';
+
+export const getByTestID = (selector: string) => {
+  return cy.get(`[data-testid="${selector}"]`);
+};
+
+export const findByTestID = (
+  subject: Cypress.PrevSubject,
+  selector: string
+) => {
+  return cy.wrap(subject).find(`[data-testid=${selector}]`);
+};
+
+export const getByTestIDStartsWith = (selector: string) => {
+  return cy.get(`[data-testid^="${selector}"]`);
+};
+
+export const interceptApi = (
+  method: 'GET' | 'POST' | 'PATCH' | 'DELETE',
+  path: string,
+  response?: RouteHandler
+) => {
+  return cy.intercept(method, `${Cypress.env('apiUrl')}${path}`, response);
+};
+
+export const loginAs = (role: User['role']) => {
+  localStorage.setItem('accessToken', faker.datatype.uuid());
+
+  return mockGetProfile({ role }).mocked;
+};
+
+Cypress.Commands.add('getByTestID', getByTestID);
+
+Cypress.Commands.add('getByTestIDStartsWith', getByTestIDStartsWith);
+
+Cypress.Commands.add('findByTestID', { prevSubject: true }, findByTestID);
+
+Cypress.Commands.add('interceptApi', interceptApi);
+
+Cypress.Commands.add('loginAs', loginAs);
