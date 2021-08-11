28c86618c7d505d9f88230f083d025b7cde4f497
Nuttavut Thongjor <n.thongjor@gmail.com>
2021-08-11 15:02:11 +0700

    setup cypress

---
+++ cypress/plugins/index.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  if (config.testingType === 'component') {
+    require('@cypress/react/plugins/react-scripts')(on, config);
+  }
+
+  return config;
+};
